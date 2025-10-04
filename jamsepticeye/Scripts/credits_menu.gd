fa7482075b40950b1ac2698f735c7ee27620f151
NICO-2\bucci <buccillinico@gmail.com>
2025-10-04 19:13:07 -0400

    Added back missing dependencies and brokeness

--- jamsepticeye/Scripts/credits_menu.gd
+++ jamsepticeye/Scripts/credits_menu.gd
@@ -1,9 +1,6 @@
 extends Control
 
-@onready var main_menu: Control = %MainMenu
+signal return_to_menu()
 
-func _on_link_button_pressed() -> void:
-	OS.shell_open("res://Assets/Videos/cat-smile.ogv")
-
-func _on_button_pressed() -> void:
-	main_menu.emit()
+func _on_return_pressed() -> void:
+	return_to_menu.emit()
