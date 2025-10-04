b06b88b3a935bde55f5d1989b0f6e337d6fcffad
GERALT\DrDut <drdutchenfoo@gmail.com>
2025-10-03 23:02:12 -0400

    Added in inputs to input map, global groups for all enemies, Gurt, rats (both player and enemy), frogs (both player and enemy), and cows (both player and enemy). All within a test folder

---
+++ jamsepticeye/TEST JOSHUA/Scripts/PlayerStates/player_gurt.gd
@@ -0,0 +1,78 @@
+extends CharacterBody2D
+
+@onready var infection_radius: Area2D = $Infection_Radius
+
+const Player_Rat = preload("res://TEST JOSHUA/Scenes/PlayerStates/player_rat.tscn")
+const Player_Frog = preload("res://TEST JOSHUA/Scenes/PlayerStates/player_frog.tscn")
+#const Player_Fox = preload()
+const Player_Cow = preload("res://TEST JOSHUA/Scenes/PlayerStates/player_cow.tscn")
+#const Player_Horse = preload()
+#const Player_Human = preload()
+
+const speed = 120.0
+var can_infect = 0
+var overlap
+var infecting_state = false
+var infested_player
+
+func _physics_process(delta: float) -> void:
+	
+	if !infecting_state:
+		var Xdirection := Input.get_axis("LEFT","RIGHT")
+		if Xdirection:
+			velocity.x = move_toward(velocity.x, (Xdirection * speed), 20)
+		else:
+			velocity.x = move_toward(velocity.x, 0, 5)
+			
+		var Ydirection := Input.get_axis("UP","DOWN")
+		if Ydirection:
+			velocity.y = move_toward(velocity.y, (Ydirection * speed), 20)
+		else:
+			velocity.y = move_toward(velocity.y, 0, 5)
+	
+	if infecting_state:
+		velocity.y = 0
+		velocity.x = 0
+	
+	move_and_slide()
+	
+	if can_infect > 0 and Input.is_action_just_pressed("Infect"):
+		var nearest_infectable
+		var shortest_distance = 1000
+		for area in infection_radius.get_overlapping_areas():
+			if area.is_in_group("Infectables"):
+				if self.position.distance_to(area.position) < shortest_distance:
+					shortest_distance = self.position.distance_to(area.position)
+					nearest_infectable = area
+		switch_character(choose_infectable(nearest_infectable), nearest_infectable.position)
+		nearest_infectable.queue_free()
+
+func switch_character(new_scene: PackedScene, position):
+	var infested_player = new_scene.instantiate()
+	infested_player.position = position
+	var parent = get_parent()
+	parent.add_child(infested_player)
+	infecting_state = true
+	self.position = Vector2(0,0)
+
+func choose_infectable(infectable):
+	if infectable.is_in_group("Rats"):
+		return Player_Rat
+	if infectable.is_in_group("Frogs"):
+		return Player_Frog
+	#if infectable.is_in_group("Foxes"):
+		#return Player_Fox
+	if infectable.is_in_group("Cows"):
+		return Player_Cow
+	#if infectable.is_in_group("Horses"):
+		#return Player_Horse
+	#if infectable.is_in_group("Humans"):
+		#return Player_Human
+
+func _on_infection_radius_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Infectables"):
+		can_infect += 1
+
+func _on_infection_radius_area_exited(area: Area2D) -> void:
+	if area.is_in_group("Infectables"):
+		can_infect -= 1 
