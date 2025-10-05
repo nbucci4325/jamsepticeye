b0660acf7297864959225fdae59324e4b534e817
DESKTOP-DAQ667N\navin <navingunaratnam77@gmail.com>
2025-10-04 20:37:18 -0400

    commiting stuff

--- jamsepticeye/TEST JOSHUA/Scripts/PlayerStates/player_gurt.gd
+++ jamsepticeye/TEST JOSHUA/Scripts/PlayerStates/player_gurt.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 @onready var infection_radius: Area2D = $Infection_Radius
 
+@onready var all_interactions = []
+@onready var label: Label = $InteractionComponets/InteractionArea/Label
+@onready var gate = get_tree().get_first_node_in_group("Gate")
+
+
 const Player_Rat = preload("res://TEST JOSHUA/Scenes/PlayerStates/player_rat.tscn")
 const Player_Frog = preload("res://TEST JOSHUA/Scenes/PlayerStates/player_frog.tscn")
 #const Player_Fox = preload()
@@ -14,8 +19,15 @@ var can_infect = 0
 var overlap
 var infecting_state = false
 var infested_player
+var gate_exsists = true
+
+func _ready():
+	update_interactions()
 
 func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("Action"):
+		execute_interaction()
 	
 	if !infecting_state:
 		var Xdirection := Input.get_axis("LEFT","RIGHT")
@@ -76,3 +88,33 @@ func _on_infection_radius_area_entered(area: Area2D) -> void:
 func _on_infection_radius_area_exited(area: Area2D) -> void:
 	if area.is_in_group("Infectables"):
 		can_infect -= 1 
+
+# Nav code - interaction
+#################################################
+
+func _on_interaction_area_area_entered(area: Area2D) -> void:
+	all_interactions.insert(0, area)
+	update_interactions()
+
+
+func _on_interaction_area_area_exited(area: Area2D) -> void:
+	all_interactions.erase(area)
+	update_interactions()
+
+func update_interactions():
+	if all_interactions:
+		label.text = all_interactions[0].interact_label
+	else:
+		label.text = ""
+
+func execute_interaction():
+	if all_interactions:
+		var cur_interaction = all_interactions[0]
+		match cur_interaction.interact_type:
+			"print_text" : print(cur_interaction.interact_value)
+			"lever" : delete_gate()
+
+func delete_gate():
+	if gate_exsists:
+		gate.queue_free()
+		gate_exsists = false
