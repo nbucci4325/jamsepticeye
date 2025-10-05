ffa50ff3ae85b7871bf794585882dbb81b0deede
TKsLappyTop\Tasha <adriantasha1999@gmail.com>
2025-10-04 21:58:57 -0400

    Added SFX/Music For Level scene

---
+++ jamsepticeye/Scripts/level.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+@onready var CaveBG = $CaveArea/CaveMusic
+@onready var CaveWD = $CaveArea/CaveWaterD
+@onready var CaveFire = $CaveArea/CaveFire
+
+
+@onready var ForestBG = $ForestArea/ForestMusic
+@onready var ForestBirds = $ForestArea/ForestBirds
+
+
+@onready var FarmBG = $FarmArea/FarmMusic
+@onready var FarmMoos = $FarmArea/FarmCows
+
+@onready var TownBG = $TownArea/TownMusic
+@onready var TownTalking = $TownArea/TownTalking
+
+
+
+
+func _on_cave_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Gurt"):
+		CaveBG.play()
+		CaveWD.play()
+		CaveFire.play()
+
+func _on_cave_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Gurt"):
+		CaveBG.stop()
+		CaveWD.stop()
+		CaveFire.stop()
+
+func _on_forest_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Gurt"):
+		ForestBG.play()
+		ForestBirds.play()
+
+func _on_forest_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Gurt"):
+		ForestBG.stop()
+		ForestBirds.stop()
+
+func _on_farm_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Gurt"):
+		FarmBG.play()
+		FarmMoos.play()
+
+func _on_farm_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Gurt"):
+		FarmBG.stop()
+		FarmMoos.stop()
+
+func _on_town_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Gurt"):
+		TownBG.play()
+		TownTalking.play()
+
+func _on_town_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Gurt"):
+		TownBG.stop()
+		TownTalking.stop()
