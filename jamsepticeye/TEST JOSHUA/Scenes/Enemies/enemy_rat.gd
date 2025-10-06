a47294ad780c2ee8b65e1c3024576c250350b958
DESKTOP-DAQ667N\navin <navingunaratnam77@gmail.com>
2025-10-05 23:12:46 -0400

    stage is doen

---
+++ jamsepticeye/TEST JOSHUA/Scenes/Enemies/enemy_rat.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+class_name Rat
+
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+
+func _physics_process(delta: float) -> void:
+	if velocity == Vector2.ZERO:
+		sprite_2d.play("Rat_Walk_Down")
+	else:
+		if abs(velocity.x) > 5:
+			sprite_2d.play("Rat_Walk")
+		else:
+			if velocity.y > 0:
+				sprite_2d.play("Rat_Walk_Down")
+			else:
+				sprite_2d.play("Rat_Walk_Up")
+
+		if velocity.x < 0:
+			sprite_2d.flip_h = false
+		else:
+			sprite_2d.flip_h = true
+	move_and_slide()
